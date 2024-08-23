9e1cbf8c74f82456b42980f431d2dd9b17e5b1c8
Victor Turcu <111446275+Klasterov@users.noreply.github.com>
2024-08-23 16:25:09 +0300

    init news

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "500px";
+    console.log(1)
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
